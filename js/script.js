ae4eebc66bced9699ab2a9c94804c78da47a0030
liyakot <janakovtun2002@gmail.com>
2022-10-26 16:10:06 +0300

    added EventListener for touch event

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const board = document.querySelector('#board');
 const colors = ['rgb(255, 127, 249)', 'rgb(84, 98, 221)', 'rgb(100, 248, 248)', 'rgb(101, 255, 121)', 'rgb(252, 255, 101)', 'rgb(255, 170, 101)', 'rgb(255, 118, 118)', 'rgb(219, 129, 255)']
-const SQYARES_NUMBER = 500;
+const squaresNumber = 500;
 
 
-for (let i = 0; i < SQYARES_NUMBER; i++) {
+for (let i = 0; i < squaresNumber; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
 
     square.addEventListener('mouseover', () => setColor(square));
+    square.addEventListener('touchstart', () => setColor(square));
     square.addEventListener('mouseleave', () => removeColor(square));
+    square.addEventListener('touchend', () => removeColor(square));
 
     board.append(square);
 }
@@ -20,7 +22,7 @@ function setColor(element) {
 }
 
 function removeColor(element) {
-    element.style.backgroundColor = 'rgb(59, 59, 59)';
+    element.style.backgroundColor = 'rgb(90, 90, 90)';
     element.style.boxShadow = `0 0 2px black`
 }
 
